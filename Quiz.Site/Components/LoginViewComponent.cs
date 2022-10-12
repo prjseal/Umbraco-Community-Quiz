862977fcda5c692be0dcc6a5fc2cf748ac3f3139
Aaron <aaron@ph-creative.com>
2022-10-12 15:01:59 +0100

    Only make hCaptcha run on staging or production

--- Quiz.Site/Components/LoginViewComponent.cs
+++ Quiz.Site/Components/LoginViewComponent.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Quiz.Site.Models;
+using Umbraco.Cms.Core.Configuration.Models;
+using IHostingEnvironment = Umbraco.Cms.Core.Hosting.IHostingEnvironment;
 
 namespace Quiz.Site.Components
 {
@@ -8,9 +10,11 @@ namespace Quiz.Site.Components
     public class LoginViewComponent : ViewComponent
     {
         private readonly hCaptchaConfiguration _hCaptchaConfiguration;
+        private readonly IHostingEnvironment _hHostingEnvironment;
 
-        public LoginViewComponent(IOptions<hCaptchaConfiguration> hCaptchaConfiguration)
+        public LoginViewComponent(IOptions<hCaptchaConfiguration> hCaptchaConfiguration, IHostingEnvironment hHostingEnvironment)
         {
+            _hHostingEnvironment = hHostingEnvironment;
             _hCaptchaConfiguration = hCaptchaConfiguration.Value;
         }
 
@@ -20,7 +24,11 @@ namespace Quiz.Site.Components
             {
                 model = new LoginViewModel();
             }
-            model.hCaptchaSiteKey = _hCaptchaConfiguration.SiteKey;
+
+            if (!_hHostingEnvironment.IsDebugMode)
+            {
+                model.hCaptchaSiteKey = _hCaptchaConfiguration.SiteKey;
+            }
             return View(model);
         }
     }
