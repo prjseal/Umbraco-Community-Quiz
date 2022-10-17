e3dd7f2ded5d0ba7eb9fe9725f28608d6afce8b8
Matthew Wise <6782865+Matthew-Wise@users.noreply.github.com>
2022-10-17 20:58:00 +0100

    Dynamic image serive for quiz results

--- Quiz.Site/Composing/RegisterServicesComposer.cs
+++ Quiz.Site/Composing/RegisterServicesComposer.cs
@@ -1,25 +1,41 @@
-﻿using Quiz.Site.Models;
-using Quiz.Site.Services;
+﻿using Quiz.Site.Services;
+using SixLabors.Fonts;
 using Umbraco.Cms.Core.Composing;
 
-namespace Quiz.Site.Composing
+namespace Quiz.Site.Composing;
+
+public class RegisterServicesComposer : IComposer
 {
-    public class RegisterServicesComposer : IComposer
+    public void Compose(IUmbracoBuilder builder)
     {
-        public void Compose(IUmbracoBuilder builder)
-        {
-            builder.Services.AddTransient<IAccountService, AccountService>();
-            builder.Services.AddTransient<IDataTypeValueService, DataTypeValueService>();
-            builder.Services.AddTransient<IMediaUploadService, MediaUploadService>();
-            builder.Services.AddTransient<IQuestionRepository, QuestionRepository>();
-            builder.Services.AddTransient<IQuizResultRepository, QuizResultRepository>();
-            builder.Services.AddTransient<IQuestionService, QuestionService>();
-            builder.Services.AddTransient<IQuizResultService, QuizResultService>();
-            builder.Services.AddTransient<IBadgeService, BadgeService>();
-            builder.Services.AddTransient<INotificationRepository, NotificationRepository>();
-            builder.Services.AddTransient<IReadNotificationRepository, ReadNotificationRepository>();
-            builder.Services.AddTransient<IhCaptchaService, hCaptchaService>();
-            builder.Services.AddTransient<IEmailBodyService, EmailBodyService>();
-        }
+        builder.Services.AddTransient<IAccountService, AccountService>();
+        builder.Services.AddTransient<IDataTypeValueService, DataTypeValueService>();
+        builder.Services.AddTransient<IMediaUploadService, MediaUploadService>();
+        builder.Services.AddTransient<IQuestionRepository, QuestionRepository>();
+        builder.Services.AddTransient<IQuizResultRepository, QuizResultRepository>();
+        builder.Services.AddTransient<IQuestionService, QuestionService>();
+        builder.Services.AddTransient<IQuizResultService, QuizResultService>();
+        builder.Services.AddTransient<IBadgeService, BadgeService>();
+        builder.Services.AddTransient<INotificationRepository, NotificationRepository>();
+        builder.Services.AddTransient<IReadNotificationRepository, ReadNotificationRepository>();
+        builder.Services.AddTransient<IhCaptchaService, hCaptchaService>();
+        builder.Services.AddTransient<IEmailBodyService, EmailBodyService>();
+
+        ConfigureDynamicImageService(builder);            
+    }
+
+    private static void ConfigureDynamicImageService(IUmbracoBuilder builder)
+    {
+        builder.Services
+            .AddSingleton<IFontCollection>(sp => {
+                FontCollection collection = new();
+                
+                var host = sp.GetRequiredService<IWebHostEnvironment>();
+                var path = host.MapPathWebRoot("/assets/fonts/OpenSans-Regular.ttf");
+                collection.Add(path);
+                return collection;
+            });
+
+        builder.Services.AddSingleton<IDynamicImageService, DynamicImageService>();
     }
 }
---
+++ Quiz.Site/Extensions/IImageProcessingContextExtensions.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace Quiz.Site.Extensions;
+
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+
+/// <summary>
+/// Taken and altered from - https://github.com/SixLabors/Samples/blob/main/ImageSharp/AvatarWithRoundedCorner/Program.cs
+/// </summary>
+public static class ImageProcessingContextExtensions
+{
+    public static IImageProcessingContext ConvertToAvatar(this IImageProcessingContext processingContext, Size size, float cornerRadius, Color color)
+    {
+        return processingContext.Resize(new ResizeOptions
+        {
+            Size = size,
+            Mode = ResizeMode.Crop,
+        }).ApplyRoundedCorners(cornerRadius, color);
+    }
+
+
+    // This method can be seen as an inline implementation of an `IImageProcessor`:
+    // (The combination of `IImageOperations.Apply()` + this could be replaced with an `IImageProcessor`)
+    private static IImageProcessingContext ApplyRoundedCorners(this IImageProcessingContext ctx, float cornerRadius, Color color)
+    {
+        Size size = ctx.GetCurrentSize();
+        IPathCollection corners = BuildCorners(size.Width, size.Height, cornerRadius);
+
+        ctx.SetGraphicsOptions(new GraphicsOptions()
+        {
+            Antialias = true,
+            AlphaCompositionMode = PixelAlphaCompositionMode.DestOut // enforces that any part of this shape that has color is punched out of the background
+        });
+
+        // mutating in here as we already have a cloned original
+        // use any color (not Transparent), so the corners will be clipped
+        foreach (var c in corners)
+        {
+            ctx = ctx.Fill(
+                new DrawingOptions()
+                {
+                    GraphicsOptions = new GraphicsOptions()
+                    {
+                        AlphaCompositionMode = PixelAlphaCompositionMode.SrcOver,
+                        Antialias = true
+                    }
+                }
+                , color, c);
+        }
+        return ctx;
+    }
+
+    private static IPathCollection BuildCorners(int imageWidth, int imageHeight, float cornerRadius)
+    {
+        // first create a square
+        var rect = new RectangularPolygon(-0.5f, -0.5f, cornerRadius, cornerRadius);
+
+        // then cut out of the square a circle so we are left with a corner
+        IPath cornerTopLeft = rect.Clip(new EllipsePolygon(cornerRadius - 0.5f, cornerRadius - 0.5f, cornerRadius));
+
+        // corner is now a corner shape positions top left
+        //lets make 3 more positioned correctly, we can do that by translating the original around the center of the image
+
+        float rightPos = imageWidth - cornerTopLeft.Bounds.Width + 1;
+        float bottomPos = imageHeight - cornerTopLeft.Bounds.Height + 1;
+
+        // move it across the width of the image - the width of the shape
+        IPath cornerTopRight = cornerTopLeft.RotateDegree(90).Translate(rightPos, 0);
+        IPath cornerBottomLeft = cornerTopLeft.RotateDegree(-90).Translate(0, bottomPos);
+        IPath cornerBottomRight = cornerTopLeft.RotateDegree(180).Translate(rightPos, bottomPos);
+
+        return new PathCollection(cornerTopLeft, cornerBottomLeft, cornerTopRight, cornerBottomRight);
+    }
+}
