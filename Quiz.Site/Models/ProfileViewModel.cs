f43c444a5e3eaf2f8843b63eede8d155cde3ec4d
Aaron <aaron@ph-creative.com>
2022-10-21 17:54:42 +0100

    Added Gravatar fallback

--- Quiz.Site/Models/ProfileViewModel.cs
+++ Quiz.Site/Models/ProfileViewModel.cs
@@ -1,5 +1,7 @@
-﻿using Umbraco.Cms.Core.Models;
+﻿using GravatarHelper.Common;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Web.Common.PublishedModels;
+using static Umbraco.Cms.Core.Constants.Conventions;
 
 namespace Quiz.Site.Models
 {
@@ -8,6 +10,24 @@ namespace Quiz.Site.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public IEnumerable<BadgePage> Badges { get; set; }
-        public MediaWithCrops Avatar { get; set; }
+        public MediaWithCrops? Avatar { get; set; }
+
+        internal string? GravatarUrl {
+            get {
+                var url = GravatarHelper.Common.GravatarHelper.CreateGravatarUrl(Email, 80, GravatarHelper.Common.GravatarHelper.DefaultImageIdenticon, GravatarRating.G, true, true, true);
+                return url;
+            }
+        }
+
+        public string? AvatarUrl {
+            get {
+                if (Avatar == null)
+                {
+                    return GravatarUrl;
+                }
+
+                return Avatar.GetCropUrl(100, 100);
+            }
+        }
     }
 }
