ad113e46ddca5b43a209a31c9c8fe94d5be4eada
Owain Williams <owaingdwilliams@gmail.com>
2022-10-27 12:05:41 +0100

    Create Content App for Used Questions

---
+++ Quiz.Site/App_Plugins/UsedQuestions/usedQuestions.resource.js
@@ -0,0 +1,14 @@
+﻿// adds the resource to umbraco.resources module:
+angular.module('umbraco').factory('usedQuestionsResource',
+    function ($http, umbRequestHelper) {
+        // the factory object returned
+        return {
+            // this calls the ApiController we setup earlier
+            getAll: function ($q) {
+                return umbRequestHelper.resourcePromise(
+                    $http.get("/Umbraco/backoffice/UsedQuestionsContentApp/UsedQuestions/GetQuestions"+"?currentNodeId="+ $q),
+                    "Failed to retrieve all question data");
+            }
+        };
+    }
+);
---
+++ Quiz.Site/Controllers/Api/UsedQuestionsController.cs
@@ -0,0 +1,47 @@
+﻿using Quiz.Site.Models;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.BackOffice.Controllers;
+using Umbraco.Cms.Web.Common.Attributes;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using Quiz.Site.Services;
+
+namespace Quiz.Site.Controllers.Api;
+
+
+[PluginController("UsedQuestionsContentApp")]
+public class UsedQuestionsController : UmbracoAuthorizedApiController
+{
+    private readonly IUmbracoContextFactory umbracoContextFactory;
+ 
+    private readonly IQuestionRepository questionRepository;
+
+
+    public UsedQuestionsController(IUmbracoContextFactory umbracoContextFactory, IQuestionRepository questionRepository)
+    {
+        this.umbracoContextFactory = umbracoContextFactory;
+        this.questionRepository = questionRepository;
+    }
+
+    public List<Question> GetQuestions(int currentNodeId)
+    {
+        List<Question> selectedQuestions = new();
+
+        using (var umbracoContextReference = umbracoContextFactory.EnsureUmbracoContext())
+        {
+            var content = umbracoContextReference.UmbracoContext?.Content?.GetById(currentNodeId);
+            
+            if(content is QuizPage quizPage && quizPage.Questions != null)
+            {
+                var arrayOfIds  = quizPage.Questions.Select(int.Parse).ToArray();
+                if (arrayOfIds != null && arrayOfIds.Any())
+                {
+                   selectedQuestions = questionRepository.GetByIds(arrayOfIds);
+
+                }
+            }
+        }
+
+        return selectedQuestions;
+
+    }
+}
