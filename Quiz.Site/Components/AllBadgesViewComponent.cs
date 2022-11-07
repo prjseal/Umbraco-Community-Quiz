aae1b1067caaa91462087e3552767bd573f9067e
Paul Seal <prjseal@gmail.com>
2022-11-07 23:47:22 +0000

    added all badges page

---
+++ Quiz.Site/Components/AllBadgesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Quiz.Site.Components
+{
+    [ViewComponent(Name = "AllBadges")]
+    public class AllBadgesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(BadgeListPage badgeListPage)
+        {
+            return View(badgeListPage);
+        }
+    }
+}
